9a6a0cf3002acf2f0c8cc75a79d398a6f06152fd
vvivksingh <vvivksingh@gmail.com>
2022-08-16 18:32:40 +0530

    login automation

---
+++ test/login.test.ts
@@ -0,0 +1,18 @@
+import { chromium } from "playwright";
+
+describe('Launch Browser', () => {
+    test('open letcode', async () => {
+        const browser = await chromium.launch({
+            headless: false
+        })
+        const context = await browser.newContext()
+        const page = await context.newPage()
+        await page.goto('https://letcode.in/');
+        await page.click("text=Log in");
+        await page.fill("input[name='email']",'vvivksingh@gmail.com');
+        await page.fill("input[name='password']",'Pass123@');
+        await page.click('button:text("LOGIN")');
+        await page.click('"Sign out"')
+        await browser.close()
+    })
+})
